19626777676a7c466918aa07ffe7dc6d04f11897
Ash <ashloraptor@gmail.com>
2024-04-29 18:39:05 -0400

    Debugging for graphQL

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,11 @@
 const typeDefs = `
   type User {
-    id: ID!
+    _id: ID!
     username: String!
     email: String!
     password: String!
-    plantCount: Int!
-    savedPlants: [Plant]!
+    plantCount: Int
+    savedPlants: [Plant]
   }
 
   type Plant {
@@ -43,7 +43,7 @@ const typeDefs = `
     user: User
     
     plants: [Plant]
-    plan(id: ID!): Plant
+    plant(id: ID!): Plant
   }
 
   type Mutation {
