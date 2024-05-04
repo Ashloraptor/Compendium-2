0cd4a610e3ebc9539d3977332aedd9afe4cb4f01
Ash <ashloraptor@gmail.com>
2024-05-04 12:16:35 -0400

    Converted to React 18 syntax

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const root = createRoot(container); 
+
+// ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,12 +8,16 @@
 //edit comments
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const root = createRoot(container); 
+
+// ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
