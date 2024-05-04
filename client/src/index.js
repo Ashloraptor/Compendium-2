d01799a1bc42c320094615eee7824565291b4b6c
Ash <ashloraptor@gmail.com>
2024-05-04 12:22:27 -0400

    Conversion was not successful. Reverting for the time being.

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const container = document.getElementById('app');
 const root = createRoot(container); 
 
 // ReactDOM.render(
