7f3768c6017cd79511d78ef56d88fbc22e1597de
Ash <ashloraptor@gmail.com>
2024-05-06 15:41:26 -0400

    Troubleshooting to deploy to Render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/PlantFinder', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/PlantFinder', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/PlantFinder', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
